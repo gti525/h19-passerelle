cc9ec218798ede986a72101be997df9c3dc399a3
komlan <sylvain.degue@gmail.com>
2019-02-02 14:21:12 -0500

    Updated transaction model

--- app/models/trasactions.py
+++ app/models/trasactions.py
@@ -1,7 +1,13 @@
+from sqlalchemy_utils import UUIDType
+
 from app import db
 
 
 class Transaction(db.Model):
+    """
+    Transaction table
+    """
+    id = db.Column(UUIDType(binary=False),primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     credit_card_number = db.Column(db.String(80), nullable=False)
@@ -9,4 +15,7 @@ class Transaction(db.Model):
     exp_year = db.Column(db.Integer, nullable=False)
     cvv = db.Column(db.Integer, nullable=False)
     amount = db.Column(db.Integer, nullable=False)
-    is_valid = db.Column(db.Boolean, default=False)
+    label = db.Column(db.String(100), nullable=False)#
+    merchant_id = db.Column(db.Integer, db.ForeignKey('merchant.id'),
+                          nullable=False)
+
