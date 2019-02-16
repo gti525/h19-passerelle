c21ae0b847bf7ebc0c930987491d3061cb9ea24c
komlan <sylvain.degue@gmail.com>
2019-02-16 09:59:32 -0500

    Update Procfile Update Update doc

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,19 +2,21 @@ from flask import jsonify
 from flask_restplus import Resource, fields, reqparse
 
 from app import api_V1
-from app.schemas import TransactionCreateSchema, TransactionConfirmCancelSchema
+from app.schemas import TransactionCreateSchema, TransactionConfirmSchema, TransactionCancelSchema
 from app.utils.decorators import parse_with, HasApiKey
 from app.utils.http_codes import *
 
 MERCHANT_API_KEY = "Merchant API_KEY"
+API_KEY = "API_KEY"
 
 tn = api_V1.namespace('transaction', description='Transaction operations')
 
 credit_card_model = tn.model('Credit Card', {
+    'first_name': fields.String(required=True, example="John"),
+    'last_name': fields.String(required=True, example="Doe"),
     'number': fields.Integer(required=True, example="356938035643809"),
     'cvv': fields.Integer(required=True, example="765"),
-    'exp_month': fields.Integer(required=True, example=10),
-    'exp_year': fields.Integer(required=True, example=22),
+    'exp': fields.String(required=True, example="10/22"),
 })
 
 merchant_model = tn.model("Merchant", {
@@ -23,20 +25,31 @@ merchant_model = tn.model("Merchant", {
 })
 
 transaction_model = tn.model('Transactions', {
-    'first_name': fields.String(required=True, example="John"),
-    'last_name': fields.String(required=True, example="Doe"),
+    API_KEY: fields.String(required=True, exammple="1234567890"),
     'amount': fields.Integer(min=0, required=True, example=100),
     'purchase_desc': fields.String(required=True, example="PURCHASE/ Simons "),
     'credit_card': fields.Nested(credit_card_model),
     'merchant': fields.Nested(merchant_model),
 })
 
-API_KEY = "API_KEY"
+# Model for transaction cancellation
+cancellation_model = tn.model('Transaction cancellation', {
+    'transaction_number': fields.String(required=True, example="1234567890"),
+    API_KEY: fields.String(required=True, example="98765431235465"),
+})
+
+# Model for transaction confirmation
+confirmation_model = tn.model('Transaction confirmation', {
+    'transaction_number': fields.String(required=True, example="1234567890"),
+    API_KEY: fields.String(required=True, example="98765431235465"),
+})
+
 # api_parser = tn.parser()
 # api_parser.add_argument(API_KEY, location="headers")
 
 api_parser = reqparse.RequestParser()
 api_parser.add_argument('API_KEY')
+
 
 @tn.route("/create")
 class TransactionResourceCreate(Resource):
@@ -45,7 +58,7 @@ class TransactionResourceCreate(Resource):
     """
 
     @tn.expect(transaction_model)
-    @tn.response(200, SUCCESS, headers={API_KEY: MERCHANT_API_KEY})
+    @tn.response(200, SUCCESS)
     @tn.response(400, INVALID)
     @tn.response(401, UNAUTHORIZED_ACCESS)
     @HasApiKey(api_parser)
@@ -54,36 +67,33 @@ class TransactionResourceCreate(Resource):
         return jsonify({"result": SUCCESS})
 
 
-@tn.route("/<int:trans_id>/confirm")
+@tn.route("/<string:trans_id>/confirm")
 class TransactionResourceConfirmation(Resource):
     """
     Endpoint for transaction confirmation
     """
 
     @HasApiKey(api_parser)
-    @tn.expect(transaction_model)
-    @tn.response(200, SUCCESS, headers={API_KEY: MERCHANT_API_KEY})
+    @tn.expect(confirmation_model)
+    @tn.response(200, SUCCESS)
     @tn.response(400, INVALID)
     @tn.response(401, UNAUTHORIZED_ACCESS)
-    @parse_with(TransactionConfirmCancelSchema(strict=True))
+    @parse_with(TransactionConfirmSchema(strict=True))
     def post(self, **kwargs):
         return jsonify({"result": SUCCESS})
 
 
-@tn.route("/<int:trans_id>/cancel")
+@tn.route("/<string:trans_id>/cancel")
 class TransactionResourceCancel(Resource):
     """
     Endpoint for transaction cancel
     """
 
     @HasApiKey(api_parser)
-    @tn.expect(transaction_model, envelope='resource')
-    @tn.response(200, SUCCESS, headers={API_KEY: MERCHANT_API_KEY})
+    @tn.expect(cancellation_model)
+    @tn.response(200, SUCCESS)
     @tn.response(400, INVALID)
     @tn.response(401, UNAUTHORIZED_ACCESS)
-    @parse_with(TransactionConfirmCancelSchema(strict=True))
+    @parse_with(TransactionCancelSchema(strict=True))
     def post(self, **kwargs):
         return jsonify({"result": SUCCESS})
-
-
-
