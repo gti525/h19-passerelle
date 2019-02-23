66e5ecb95f7e1f6229b74b6be0b357357cf9c0a1
komlan <sylvain.degue@gmail.com>
2019-02-23 12:09:18 -0500

    Added encryption Update transaction

---
+++ tests/test_aes.py
@@ -0,0 +1,16 @@
+import pytest
+
+from app.utils.aes import decrypt, encrypt
+
+
+class TestAES(object):
+
+    @pytest.mark.parametrize("text", [
+        ("Je suis le plus grand"),
+        ("I currently have 4 windows open up… and I don’t know why."),
+        ("Everyone was busy, so I went to the movie alone."),
+        ("She only paints with bold colors; she does not like pastels"),
+        ("He didn’t want to go to the dentist, yet he went anyway.")
+    ])
+    def test_encryption(self, text):
+        assert text == decrypt(encrypt(text))
