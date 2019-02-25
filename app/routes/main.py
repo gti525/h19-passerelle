96c3f3d05f61111ed81d3a319f02c44ea4851e59
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2019-02-25 18:36:24 -0500

    route issue should pass now

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,6 +3,6 @@ from flask import render_template, Blueprint
 main_bp = Blueprint('main', __name__, url_prefix='/index')
 
 
-@main_bp.route("/")
+@main_bp.route("")
 def index():
     return render_template('index.html', title='Home')
