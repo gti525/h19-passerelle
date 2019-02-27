7f3b914cf1f3d57e09192cee13d064dd9926240e
Sylvain Dégué <sylvain.degue@gmail.com>
2019-02-26 23:39:39 -0500

    Added status to transaction Update api documentation
    asdsdag
    
    asdsdag

--- app/models/trasactions.py
+++ app/models/trasactions.py
@@ -1,11 +1,16 @@
 from app import db
 
+PENDING = "Pending"
+AUTHORIZED = "Authorized"
+REFUSED = "Refused"
+VERIFIED = "Verified"
+
 
 class Transaction(db.Model):
     """
     Transaction table
     """
-    id = db.Column(db.String(30) ,primary_key=True)
+    id = db.Column(db.String(30), primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     credit_card_number = db.Column(db.String(80), nullable=False)
@@ -14,5 +19,5 @@ class Transaction(db.Model):
     amount = db.Column(db.Integer, nullable=False)
     label = db.Column(db.String(100), nullable=False)
     merchant_id = db.Column(db.Integer, db.ForeignKey('merchant.id'),
-                          nullable=False)
-
+                            nullable=False)
+    status = db.Column(db.String(100), nullable=False, default=PENDING)
