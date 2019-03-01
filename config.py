0082ab73cfa84063f020e4c59db4c7ef40a562c0
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2019-03-01 16:45:50 -0500

    reverse le changement

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class ProductionConfig(Config):
