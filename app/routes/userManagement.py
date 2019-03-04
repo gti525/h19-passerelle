29af4af237820aaa9d4901859ca6602f15de391d
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2019-03-04 16:08:41 -0500

    user creation supports account number merchant modification merchant deactivation better flash use

--- app/routes/userManagement.py
+++ app/routes/userManagement.py
@@ -1,5 +1,7 @@
 from flask_login import current_user
-from flask import render_template, Blueprint, redirect
+from flask import render_template, Blueprint, redirect, request, flash
+from app.models.users import Merchant
+from app import db
 
 userManagement_bp = Blueprint('userManagement', __name__, url_prefix='/userManagement')
 
@@ -7,6 +9,28 @@ userManagement_bp = Blueprint('userManagement', __name__, url_prefix='/userManag
 @userManagement_bp.route("/", methods=['GET', 'POST'])
 def userManagement():
     if current_user.is_authenticated:
-        return render_template("userManagement.html")
+        if current_user.type == 'admin':
+            merchants = Merchant.query.filter_by(status='active')
+        else:
+            redirect("dashboard.html")
+
+        return render_template("userManagement.html", merchants=merchants)
+    else:
+        return redirect('login')
+
+
+@userManagement_bp.route("/delete", methods=['GET'])
+def userDelete():
+    if current_user.is_authenticated:
+        if current_user.type == 'admin':
+            merchantId = request.args.get('merchantId')
+            merchant = Merchant.query.filter_by(id=merchantId).first()
+            merchant.status = 'inactive'
+            db.session.commit()
+            flash("Marchand supprimer")
+        else:
+            redirect("dashboard")
+
+        return redirect('userManagement')
     else:
         return redirect('login')
