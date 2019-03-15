36f2785f7b4ffda05cd79ef0bb40b7d1b60b3e2f
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2019-03-15 12:12:34 -0400

    merchant transactions by status

--- app/routes/transaction.py
+++ app/routes/transaction.py
@@ -29,7 +29,18 @@ def transaction():
         transactionsByMerchant[merchant01.name] = Transaction.query.filter_by(merchant_id=vente01_id).count()
         transactionsByMerchant[merchant02.name] = Transaction.query.filter_by(merchant_id=vente02_id).count()
 
-        return render_template("transaction.html", type=current_user.type, transactions=transactions, recentTransactions=recentTransactions,
-                               transactionsByMerchant=transactionsByMerchant)
+        transactionByStatus = []
+        transactionByStatus.append(len([transaction for transaction in transactions if
+                                        transaction.status in "Pending"]))
+        transactionByStatus.append(len([transaction for transaction in transactions if
+                                        transaction.status in "Authorized"]))
+        transactionByStatus.append(len([transaction for transaction in transactions if
+                                        transaction.status in "Refused"]))
+        transactionByStatus.append(len([transaction for transaction in transactions if
+                                        transaction.status in "Verified"]))
+
+        return render_template("transaction.html", type=current_user.type, transactions=transactions,
+                               recentTransactions=recentTransactions,
+                               transactionsByMerchant=transactionsByMerchant, transactionByStatus=transactionByStatus)
     else:
         return redirect('login')
