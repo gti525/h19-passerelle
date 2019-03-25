c8817fed3522dfc524140b28245ed4c2d25d6048
komlan55 <sylvain.degue@gmail.com>
2019-03-25 18:55:11 -0400

    Fix app context error

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from app import create_app
 
 config = os.getenv('APP_SETTINGS') # config_name = config.DevelopmentConfig
 app = create_app(config)
+app.app_context().push()
 
 if __name__ == '__main__':
     app.run()
