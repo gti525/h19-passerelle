961d6b25187255e40a96dd03153fbdde896d59ec
Sylvain Dégué <sylvain.degue@gmail.com>
2019-03-30 18:13:55 -0400

    Update invalid message

--- app/consts.py
+++ app/consts.py
@@ -1,6 +1,6 @@
 SUCCESS = "SUCCESS"
-INVALID = "INVALID REQUEST"
-UNAUTHORIZED_ACCESS = "UNAUTHORIZED ACCESS. Check that MERChANT_API_KEY is in the request"
+INVALID = "INVALID REQUEST. Verify your payload"
+UNAUTHORIZED_ACCESS = "UNAUTHORIZED ACCESS. Check that MERCHANT_API_KEY is in the request"
 MERCHANT_API_KEY = "MERCHANT_API_KEY"
 BANK1_BASE_URL = "https://banque1-test.herokuapp.com"
 BANK2_BASE_URL = "https://banque2-h19.herokuapp.com"
